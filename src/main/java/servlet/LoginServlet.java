c269fa4dd50bcfe2ea5bc5aef49be63166427add
liaobonn <liaobonn@ecs.vuw.ac.nz>
2017-05-16 16:28:53 +1200

    Trying different way of sending json

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -2,8 +2,8 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.json.Json;
-import javax.json.JsonObject;
+//import javax.json.Json;
+//import javax.json.JsonObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -22,11 +22,12 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
     	PrintWriter write = resp.getWriter();
-    	JsonObject o = Json.createObjectBuilder()
-    			.add("id_token", 124243)
-    			.add("data", "You got to the login route! :D")
-    			.build();
-    	write.print(o.toString());
+//    	JsonObject o = Json.createObjectBuilder()
+//    			.add("id_token", 124243)
+//    			.add("data", "You got to the login route! :D")
+//    			.build();
+//    	write.print(o.toString());
+    	writer.write('{"id_token" : 12345, "data": "Hello world"}');
     	write.flush();
     	write.close();
     }
