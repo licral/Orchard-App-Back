f7131a3977891861a205e8923bf0a702f5e49a6c
liaobonn <liaobonn@ecs.vuw.ac.nz>
2017-05-16 16:33:20 +1200

    Testing if this sends

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -2,8 +2,6 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-//import javax.json.Json;
-//import javax.json.JsonObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -22,12 +20,7 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
     	PrintWriter write = resp.getWriter();
-//    	JsonObject o = Json.createObjectBuilder()
-//    			.add("id_token", 124243)
-//    			.add("data", "You got to the login route! :D")
-//    			.build();
-//    	write.print(o.toString());
-    	write.write('{"id_token" : 12345, "data": "Hello world"}');
+    	write.write('{id_token : 12345, data: "Hello world"}');
     	write.flush();
     	write.close();
     }
