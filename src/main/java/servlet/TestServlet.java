38854edf0a9167d5d7390addcf887bc1df321a5a
licral <kurousagiwasugoi@gmail.com>
2017-05-17 12:35:46 +1200

    imported library for the exception handling

--- src/main/java/servlet/TestServlet.java
+++ src/main/java/servlet/TestServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
 
 @WebServlet(
         name = "TestServlet",
