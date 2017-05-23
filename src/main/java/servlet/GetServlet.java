1da62a7f7c9bb96ae93dbc8f30b74122e2249eca
liaobonn <liaobonn@ecs.vuw.ac.nz>
2017-05-23 18:32:09 +1200

    Added a get route

---
+++ src/main/java/servlet/GetServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.MacProvider;
+import java.security.Key;
+
+@WebServlet(
+        name = "GetServlet",
+        urlPatterns = {"/get/*"}
+    )
+public class GetServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+    	String service = req.getPathInfo().substring(1);
+    	System.out.println(service);
+
+    }
+
+}
