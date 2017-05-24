6411d3f3a1f26b6fd4a8a5bdf81237d4a17b5d25
licral <kurousagiwasugoi@gmail.com>
2017-05-24 14:58:11 +1200

    Made a register service. So far checks for username

---
+++ src/main/java/servlet/RegisterServlet.java
@@ -0,0 +1,130 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.io.PrintWriter;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureException;
+
+@WebServlet(
+        name = "RegisterServlet",
+        urlPatterns = {"/register"}
+    )
+public class RegisterServlet extends HttpServlet {
+	private String username;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+    	if(!isAuthorised(req.getHeader("Authorization"))){
+    		resp.sendError(400);
+    	} else {
+    		System.out.println(username);
+	    }
+    }
+
+    private boolean isAuthorised(String token) throws ServletException{
+    	try{
+    		Claims claims = Jwts.parser().setSigningKey("secret".getBytes("UTF-8")).parseClaimsJws(token).getBody();
+
+    		String username = claims.get("username").toString();
+
+    		if(username == null){
+        		return false;
+        	} else {
+        		Connection con = (Connection)getServletContext().getAttribute("DBConnection");
+            	PreparedStatement ps = null;
+        		ResultSet rs = null;
+        		try {
+        			ps = con.prepareStatement("select organisation_id from users where token=?");
+        			ps.setString(1, token);
+        			rs = ps.executeQuery();
+        			if(rs != null && rs.next()){
+        				if(username.equals(rs.getString("organisation_id"))){
+        					this.username = username;
+        					return true;
+        				} else {
+        					return false;
+        				}
+        			}else{
+        				System.out.println("No results");
+        				return false;
+        			}
+        		} catch (SQLException e) {
+        			e.printStackTrace();
+        			System.out.println("Database connection problem");
+        			throw new ServletException("DB Connection problem.");
+        		}finally{
+        			try {
+        				rs.close();
+        				ps.close();
+        			} catch (SQLException e) {
+        				System.out.println("SQLException in closing PreparedStatement or ResultSet");
+        			}
+        		}
+        	}
+    	} catch (SignatureException e) {
+    		System.out.print(e.getMessage());
+			return false;
+    	} catch (UnsupportedEncodingException e){
+    		System.out.print(e.getMessage());
+    		return false;
+    	}
+    }
+
+    private void getSpecies(HttpServletResponse resp) throws ServletException, IOException {
+    	Connection con = (Connection)getServletContext().getAttribute("DBConnection");
+    	PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = con.prepareStatement("select * from species");
+			rs = ps.executeQuery();
+			String speciesArray = "{";
+			if(rs != null && rs.next()){
+				do{
+					speciesArray += "\"" + rs.getString("species_id") + "\":\"" + rs.getString("species") + "\"";
+					if(!rs.isLast()){
+						speciesArray += ",";
+					}
+
+				} while(rs.next());
+				speciesArray += "}";
+
+				PrintWriter write = resp.getWriter();
+				write.write(speciesArray);
+    	    	write.flush();
+    	    	write.close();
+			}else{
+				System.out.println("No results");
+				resp.sendError(400);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Database connection problem");
+			throw new ServletException("DB Connection problem.");
+		}finally{
+			try {
+				rs.close();
+				ps.close();
+			} catch (SQLException e) {
+				System.out.println("SQLException in closing PreparedStatement or ResultSet");
+			}
+
+		}
+    }
+
+}
