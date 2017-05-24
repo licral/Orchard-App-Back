1b84bc5b6f466aa76981bc2648c345a5a3d184ea
licral <kurousagiwasugoi@gmail.com>
2017-05-24 19:57:50 +1200

    Added a record servlet

---
+++ src/main/java/servlet/RecordServlet.java
@@ -0,0 +1,134 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.io.PrintWriter;
+
+import java.lang.NumberFormatException;
+import java.lang.IllegalArgumentException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureException;
+
+@WebServlet(
+        name = "RecordServlet",
+        urlPatterns = {"/record/*"}
+    )
+public class RecordServlet extends HttpServlet {
+	private String organisation_id;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+    	if(!isAuthorised(req.getHeader("Authorization"))){
+    		resp.sendError(400);
+    	} else {
+            System.out.println("Got to record servlet!");
+   //  		String plant_id = req.getParameter("plant_id");
+   //  		String visual_tag = req.getParameter("visual_tag");
+   //  		String notes = req.getParameter("notes");
+   //  		try{
+   //  			int variety_id = Integer.parseInt(req.getParameter("variety_id"));
+   //  			float longitude = Float.parseFloat(req.getParameter("longitude"));
+   //  			float latitude = Float.parseFloat(req.getParameter("latitude"));
+   //  			Date date = Date.valueOf(req.getParameter("date"));
+
+			// 	Connection con = (Connection)getServletContext().getAttribute("DBConnection");
+		 //    	PreparedStatement ps = null;
+			// 	try {
+			// 		ps = con.prepareStatement("insert into plant_record (plant_id, visual_tag, variety_id, organisation_id, longitude, latitude, date, notes) values (?, ?, ?, ?, ?, ?, ?, ?)");
+			// 		ps.setString(1, plant_id);
+			// 		ps.setString(2, visual_tag);
+			// 		ps.setInt(3, variety_id);
+			// 		ps.setString(4, organisation_id);
+			// 		ps.setFloat(5, longitude);
+			// 		ps.setFloat(6, latitude);
+			// 		ps.setDate(7, date);
+			// 		ps.setString(8, notes);
+			// 		ps.execute();
+			// 		resp.setStatus(200);
+			// 	} catch (SQLException e) {
+			// 		e.printStackTrace();
+			// 		System.out.println("Database connection problem");
+			// 		throw new ServletException("DB Connection problem.");
+			// 	}finally{
+			// 		try {
+			// 			ps.close();
+			// 		} catch (SQLException e) {
+			// 			System.out.println("SQLException in closing PreparedStatement or ResultSet");
+			// 		}
+
+			// 	}
+   //  		} catch (NumberFormatException e){
+   //  			System.out.println(e.getMessage());
+   //  			resp.sendError(400);
+   //  		} catch (IllegalArgumentException e){
+			// 	System.out.println(e.getMessage());
+			// 	resp.sendError(400);
+			// }
+	    }
+    }
+
+    private boolean isAuthorised(String token) throws ServletException{
+    	try{
+    		Claims claims = Jwts.parser().setSigningKey("secret".getBytes("UTF-8")).parseClaimsJws(token).getBody();
+
+    		String username = claims.get("username").toString();
+
+    		if(username == null){
+        		return false;
+        	} else {
+        		Connection con = (Connection)getServletContext().getAttribute("DBConnection");
+            	PreparedStatement ps = null;
+        		ResultSet rs = null;
+        		try {
+        			ps = con.prepareStatement("select organisation_id from users where token=?");
+        			ps.setString(1, token);
+        			rs = ps.executeQuery();
+        			if(rs != null && rs.next()){
+        				if(username.equals(rs.getString("organisation_id"))){
+        					this.organisation_id = username;
+        					return true;
+        				} else {
+        					return false;
+        				}
+        			}else{
+        				System.out.println("No results");
+        				return false;
+        			}
+        		} catch (SQLException e) {
+        			e.printStackTrace();
+        			System.out.println("Database connection problem");
+        			throw new ServletException("DB Connection problem.");
+        		}finally{
+        			try {
+        				rs.close();
+        				ps.close();
+        			} catch (SQLException e) {
+        				System.out.println("SQLException in closing PreparedStatement or ResultSet");
+        			}
+        		}
+        	}
+    	} catch (SignatureException e) {
+    		System.out.print(e.getMessage());
+			return false;
+    	} catch (UnsupportedEncodingException e){
+    		System.out.print(e.getMessage());
+    		return false;
+    	}
+    }
+}
