0da0902b126c9ec0ce0034fdf0b2cde46b984e4a
liaobonn <liaobonn@ecs.vuw.ac.nz>
2017-05-29 15:29:57 +1200

    Edited the array to be sent

--- src/main/java/servlet/HistoryServlet.java
+++ src/main/java/servlet/HistoryServlet.java
@@ -111,7 +111,7 @@ public class HistoryServlet extends HttpServlet {
             String activityArray = "[";
             if(rs != null && rs.next()){
                 do{
-                    activityArray += "\"" + rs.getInt("activity_id") + "\":{\"plant_id\":\"" + rs.getString("plant_id") + "\", \"date\":\"" + rs.getDate("date") + "\", \"time\":\"" + rs.getTime("time") + "\", \"activity_id\":\"" + rs.getInt("activity_id") + "\", \"activity_type\":\"" + rs.getString("activity_type") + "\", \"species\":\"" + rs.getString("species") + "\"}";
+                    activityArray += "{\"plant_id\":\"" + rs.getString("plant_id") + "\", \"date\":\"" + rs.getDate("date") + "\", \"time\":\"" + rs.getTime("time") + "\", \"activity_id\":\"" + rs.getInt("activity_id") + "\", \"activity_type\":\"" + rs.getString("activity_type") + "\", \"species\":\"" + rs.getString("species") + "\"}";
                     if(!rs.isLast()){
                         activityArray += ",";
                     }
